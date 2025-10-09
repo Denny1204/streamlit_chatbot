659bbc1b6d151ef19e429765d199cee8c04300a5
Denny1204 <93825565+Denny1204@users.noreply.github.com>
2025-10-09 19:22:52 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="AI Interviewer", page_icon="🤖")
 
 st.sidebar.title("Navigasi")
-st.sidebar.page_link("app/pages/interview_chatbot.py", label="Wawancara AI")
+st.sidebar.page_link("app/interview_chatbot.py", label="Wawancara AI")
 # nanti bisa tambah halaman lain: login, hasil analisis, dashboard, dll
